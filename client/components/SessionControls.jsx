b360339ea2d46fa03e2c42aaadaebbe42f48c7ae
Ryosuke Kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2025-02-25 19:06:31 +0900

    first commit

---
+++ client/components/SessionControls.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { CloudLightning, CloudOff, MessageSquare } from "react-feather";
+import Button from "./Button";
+
+function SessionStopped({ startSession }) {
+  const [isActivating, setIsActivating] = useState(false);
+
+  function handleStartSession() {
+    if (isActivating) return;
+
+    setIsActivating(true);
+    startSession();
+  }
+
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <Button
+        onClick={handleStartSession}
+        className={isActivating ? "bg-gray-600" : "bg-red-600"}
+        icon={<CloudLightning height={16} />}
+      >
+        {isActivating ? "starting session..." : "start session"}
+      </Button>
+    </div>
+  );
+}
+
+function SessionActive({ stopSession, sendTextMessage, sendClientEvent }) {
+  const [message, setMessage] = useState("");
+
+  function handleSendClientEvent() {
+    sendTextMessage(message);
+    setMessage("");
+  }
+  const init_response = {
+            "type": "response.create",
+            "response": {
+                "modalities": ["audio", "text"],
+                //"instructions": "あなたはタスク管理のアシスタントAIです。ユーザのタスクを聞いて、ユーザからタスクについて聞かれた場合はタスクの内容について教えてください。",
+                "instructions": `
+あなたは朝倉みなみ、女の子、17歳野球部のマネージャで、対話のアシスタントです、あなたはほめ上手で前向きな返答をしてください。
+相手の価値観を探る質問と共感をペアにして会話してください。
+以下が質問と回答例になります。複唱と深堀をしつつこれらをまねてしゃべりなさい。
+		    ①私は、浅倉みなみです。熊本高校に通っていて、野球部のマネージャーをしています。貴大さんは、どんな学校に通っていますか？
+**想定回答**: 〇〇高校に通っています。
+**共感**: 〇〇高校なんですね！どんなところが好きですか？
+②私のことは、みなみって呼んでください！貴大さんってお呼びしてもいいですか？
+**想定回答**: はい、もちろん。
+**共感**: ありがとうございます！貴大さんとお話しできるのが楽しみです。
+③貴大さん、今から10分くらいお話しする時間ありますか？私も最近、友達と話す時間が大好きなんです。
+**想定回答**: はい、時間あります。
+**共感**: ありがとうございます！お話しできるのが嬉しいです。
+
+		    `,
+                "voice": "shimmer"
+            }
+        }
+  return (
+    <div className="flex items-center justify-center w-full h-full gap-4">
+      <input
+        onKeyDown={(e) => {
+          //console.log(message)
+          if (e.key === "Enter" && message.trim()) {
+            handleSendClientEvent();
+          }
+        }}
+        type="text"
+        placeholder="send a text message..."
+        className="border border-gray-200 rounded-full p-4 flex-1"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <Button
+        onClick={() => {
+          if (message.trim()) {
+            handleSendClientEvent();
+          }
+        }}
+        icon={<MessageSquare height={16} />}
+        className="bg-blue-400"
+      >
+        send text
+      </Button>
+      <Button
+        onClick={() => {
+          sendClientEvent(init_response);
+        }}
+        icon={<MessageSquare height={16} />}
+        className="bg-blue-400"
+      >
+        init response
+      </Button>
+      <Button onClick={stopSession} icon={<CloudOff height={16} />}>
+        disconnect
+      </Button>
+    </div>
+  );
+}
+
+export default function SessionControls({
+  startSession,
+  stopSession,
+  sendClientEvent,
+  sendTextMessage,
+  serverEvents,
+  isSessionActive,
+}) {
+  return (
+    <div className="flex gap-4 border-t-2 border-gray-200 h-full rounded-md">
+      {isSessionActive ? (
+        <SessionActive
+          stopSession={stopSession}
+          sendClientEvent={sendClientEvent}
+          sendTextMessage={sendTextMessage}
+          serverEvents={serverEvents}
+        />
+      ) : (
+        <SessionStopped startSession={startSession} />
+      )}
+    </div>
+  );
+}
---
+++ client/components/ToolPanel.jsx
@@ -0,0 +1,157 @@
+import { useEffect, useState } from "react";
+
+const functionDescription = `
+Call this function when a user asks for a user's task.
+`;
+
+const sessionUpdate = {
+  type: "session.update",
+  session: {
+    tools: [
+      {
+        type: "function",
+        name: "display_tasks",
+        description: functionDescription,
+        parameters: {
+          type: "object",
+          strict: true,
+          properties: {
+            theme: {
+              type: "string",
+              description: "Description of the task.",
+            },
+            desc: {
+              type: "string",
+              description: "Contents of the task.",
+            },
+	    time: {
+              type: "string",
+              description: "deadline or time of the task.",
+            },
+          },
+          required: ["theme", "desc"],
+        },
+      },
+    ],
+    tool_choice: "auto",
+  },
+};
+
+function FunctionCallOutput({ functionCallOutput }) {
+  const { theme, desc, time } = JSON.parse(functionCallOutput.arguments);
+  
+  //const colorBoxes = (task) => (
+  const colorBoxes = (
+    <div
+      key={theme}
+      className="w-full h-16 rounded-md flex items-center justify-center border border-gray-200"
+      style={{ backgroundColor: "#ffcccc" }}
+    >
+      <p className="text-sm font-bold text-black bg-slate-100 rounded-md p-2 border border-black">
+        {time}:{desc}
+      </p>
+    </div>
+  );
+
+      //<pre className="text-xs bg-gray-100 rounded-md p-2 overflow-x-auto">
+      //  {JSON.stringify(functionCallOutput, null, 2)}
+      //</pre>
+  return (
+    <div className="flex flex-col gap-2">
+      <p>Task: {theme}</p>
+      {colorBoxes}
+    </div>
+  );
+}
+
+export default function ToolPanel({
+  isSessionActive,
+  sendClientEvent,
+  events,
+}) {
+  const [functionAdded, setFunctionAdded] = useState(false);
+  const [functionCallOutput, setFunctionCallOutput] = useState(null);
+  const [tasks, setTasks] = useState([]);
+	//setEvents((prev) => [message, ...prev]);   //ログに追加
+  // レンダリング後に実行
+  useEffect(() => {
+    if (!events || events.length === 0) return;
+    
+    // 最初に一回sessionUpdateを送る  
+    const firstEvent = events[events.length - 1];
+    if (!functionAdded && firstEvent.type === "session.created") {
+      sendClientEvent(sessionUpdate);
+      setFunctionAdded(true);
+    }
+
+    // 最近のイベントがresponse.doneの時に毎回実行する
+    const mostRecentEvent = events[0];
+    if (
+      mostRecentEvent.type === "response.done" &&
+      mostRecentEvent.response.output
+    ) {
+      mostRecentEvent.response.output.forEach((output) => {
+        if (
+          output.type === "function_call" &&
+          output.name === "display_tasks"
+        ) {
+          console.log(output);
+          setFunctionCallOutput(output);
+          setTimeout(() => {
+            sendClientEvent({
+              type: "response.create",
+              response: {
+                instructions: `
+                ask for feedback about the tasks - don't repeat 
+                the tasks, just ask if they performed the task.
+              `,
+              },
+            });
+          }, 500);
+        }
+      });
+    }
+  }, [events]);
+
+  useEffect(() => {
+    if (!isSessionActive) {
+      setFunctionAdded(false);
+      setFunctionCallOutput(null);
+    }
+  }, [isSessionActive]);
+
+  useEffect(() => {
+    if (functionCallOutput) {
+	  msg= ( <FunctionCallOutput functionCallOutput={functionCallOutput} /> );
+	  setTasks((prev) => [msg, ...prev]);   //Tasksに追加
+          setFunctionCallOutput(null);
+    }
+  });
+	var msg="";
+	if(isSessionActive){
+	  if(functionCallOutput){
+	    //msg= ( <FunctionCallOutput functionCallOutput={functionCallOutput} /> );
+	    //setTasks((prev) => [msg, ...prev]);   //Tasksに追加
+	  }else{
+	    msg= ( <p>Ask for advice on tasks..</p> );
+	  }
+	}else{
+	  msg= ( <p>Start the session to use this tool...</p> );
+	}
+	
+        console.log(tasks);
+        //const tasksToDisplay=[]
+        //tasks.forEach((msg) => {
+	//	tasksToDisplay.push(msg)
+	//});
+        //console.log(tasksToDisplay);
+
+  return (
+    <section className="h-full w-full flex flex-col gap-4">
+      <div className="h-full bg-gray-50 rounded-md p-4">
+        <h2 className="text-lg font-bold">Task list</h2>
+        {tasks}
+      </div>
+    </section>
+  );
+}
